965d1cf7af0f429db01b7aa4f42b968ea5bfd14a
Elena <56148816+elenajusto@users.noreply.github.com>
2024-09-03 13:55:45 +1000

    moved sensor initiliasations to own functions

--- hydra-fc/src/main.cpp
+++ hydra-fc/src/main.cpp
@@ -16,6 +16,9 @@ File root;
 
 /* ------------- Function Prototypes ------------- */
 void i2cscan();
+void initImu();
+void initBaro();
+void initSd();
 void printBarometer();
 void printImu();
 void printDirectory(File dir, int numTabs);
@@ -29,54 +32,16 @@ void setup() {
   while (!Serial);        
 
   // Initialise IMU 
-  Serial.println("Initialising IMU.");  
-  if (bmi160.softReset() != BMI160_OK){
-    Serial.println("reset false");
-    while(1);
-  }
-  if (bmi160.I2cInit(i2c_addr) != BMI160_OK){
-    Serial.println("init false");
-    while(1);
-  }
-  Serial.println("IMU Initialised.");
+  initImu();
 
   // Initialising Barometer
-  Serial.println("Initialising barometer.");
-  unsigned status;
-  status = bmp.begin(0x76);
-
-  if (!status) {
-    Serial.println(F("Could not find a valid BMP280 sensor, check wiring or "
-                      "try a different address!"));
-    while (1) delay(10);
-  }
-  bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     /* Operating Mode. */
-                  Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
-                  Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
-                  Adafruit_BMP280::FILTER_X16,      /* Filtering. */
-                  Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
-  Serial.println("Barometer intialised.\n");
+  initBaro();
 
   // Initilise SD Card
-  Serial.print("Initializing SD card...");
-
-  if (!SD.begin(chipSelect)) {
-    Serial.println("initialization failed. Things to check:");
-    Serial.println("1. is a card inserted?");
-    Serial.println("2. is your wiring correct?");
-    Serial.println("3. did you change the chipSelect pin to match your shield or module?");
-    Serial.println("Note: press reset button on the board and reopen this Serial Monitor after fixing your issue!");
-    while (true);
-  }
-
-  Serial.println("initialization done.");
+  initSd();
 
   root = SD.open("/");
-
   printDirectory(root, 0);
-
-  Serial.println("done!");
-
 }
 
 void loop() {
@@ -182,4 +147,49 @@ void printDirectory(File dir, int numTabs) {
     }
     entry.close();
   }
+}
+
+void initImu(){
+  Serial.println("Initialising IMU.");  
+  if (bmi160.softReset() != BMI160_OK){
+    Serial.println("reset false");
+    while(1);
+  }
+  if (bmi160.I2cInit(i2c_addr) != BMI160_OK){
+    Serial.println("init false");
+    while(1);
+  }
+  Serial.println("IMU Initialised.");
+}
+
+void initBaro(){
+  Serial.println("Initialising barometer.");
+  unsigned status;
+  status = bmp.begin(0x76);
+
+  if (!status) {
+    Serial.println(F("Could not find a valid BMP280 sensor, check wiring or "
+                      "try a different address!"));
+    while (1) delay(10);
+  }
+  bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     /* Operating Mode. */
+                  Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
+                  Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
+                  Adafruit_BMP280::FILTER_X16,      /* Filtering. */
+                  Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
+  Serial.println("Barometer intialised.\n");
+}
+
+void initSd(){
+  Serial.print("Initialising SD card.\n");
+
+  if (!SD.begin(chipSelect)) {
+    Serial.println("initialization failed. Things to check:");
+    Serial.println("1. is a card inserted?");
+    Serial.println("2. is your wiring correct?");
+    Serial.println("3. did you change the chipSelect pin to match your shield or module?");
+    Serial.println("Note: press reset button on the board and reopen this Serial Monitor after fixing your issue!");
+    while (true);
+  }
+  Serial.println("SD Card initialised.\n");
 }
